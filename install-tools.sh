4f5f7458e7b129d9ad6b82a58c15e358b5f57bec
Ankit <43137695+andalike@users.noreply.github.com>
2023-09-13 22:45:09 +0530

    Install Tools Done

--- install-tools.sh
+++ install-tools.sh
@@ -2,15 +2,11 @@
 wget -q terraform.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
 unzip terraform_1.5.0_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-chmod +x /usr/local/bin/terraform
 
 # Install TfLint
 wget -q tflint.zip https://github.com/terraform-linters/tflint/releases/download/v0.47.0/tflint_linux_amd64.zip
 unzip tflint_linux_amd64.zip
 sudo mv tflint /usr/local/bin/
-chmod +x /usr/local/bin/tflint
 
 # Install Tfsec
-wget -q tfsec.zip https://github.com/aquasecurity/tfsec/releases/download/v1.28.1/tfsec-checkgen-linux-amd64
-sudo mv tfsec-checkgen-linux-amd64 /usr/local/bin/
-chmod +x /usr/local/bin/tfsec
+curl -s https://raw.githubusercontent.com/aquasecurity/tfsec/master/scripts/install_linux.sh | bash
