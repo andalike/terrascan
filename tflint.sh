06be7a558f93a2ff3bf034b40a8c3063271138dc
Ankit <43137695+andalike@users.noreply.github.com>
2023-09-13 23:43:45 +0530

    Update tflint.sh

--- tflint.sh
+++ tflint.sh
@@ -3,5 +3,11 @@
 set -e
 set -x
 
-tflint --init
-tflint --recursive --config "$(pwd)/.tflint.hcl"
+INSTALL_DIR="$1"
+
+# Debugging: Print the contents of the installation directory
+ls -l "$INSTALL_DIR"
+
+# Run tflint with the correct path
+"${INSTALL_DIR}/tflint" --init
+"${INSTALL_DIR}/tflint" --recursive --config "$(pwd)/.tflint.hcl"
