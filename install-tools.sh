7e15612d17a7f3eebcecefbbb86e2d62d7f9939d
Ankit <43137695+andalike@users.noreply.github.com>
2023-09-13 23:36:50 +0530

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -1,16 +1,13 @@
+INSTALL_DIR="$1"
 # Install Terraform
 wget -qO terraform.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
-unzip terraform.zip
-mv terraform /usr/local/bin/
-chmod +x /usr/local/bin/terraform
+unzip terraform.zip -d "${INSTALL_DIR}"
 
 # Install TfLint
 wget -qO tflint.zip https://github.com/terraform-linters/tflint/releases/download/v0.47.0/tflint_linux_amd64.zip
-unzip tflint.zip
-mv tflint /usr/local/bin/
-chmod +x /usr/local/bin/tflint
+unzip tflint.zip -d "${INSTALL_DIR}"
+
 
 # Install Tfsec
 wget -qO tfsec https://github.com/aquasecurity/tfsec/releases/download/v1.28.1/tfsec-checkgen-linux-amd64
-mv tfsec /usr/local/bin/
-chmod +x /usr/local/bin/tfsec
+unzip tfsec -d "${INSTALL_DIR}"
