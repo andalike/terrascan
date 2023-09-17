cd9e4e1ead5419e39dafae8e0da13a6dfe862425
Ankit <43137695+andalike@users.noreply.github.com>
2023-09-17 15:15:34 +0530

    Update tfsec-scan.sh

--- tfsec-scan.sh
+++ tfsec-scan.sh
@@ -2,8 +2,11 @@
 
 set +e
 
-tfsec -f junit,default --out tfsec --config-file tfsec.yaml --no-color --include-passed
+# Full path to tfsec executable
+TFSEC_EXECUTABLE="${WORKSPACE}/tools/tfsec"
 
+# Run tfsec using the full path
+$TFSEC_EXECUTABLE -f junit,default --out tfsec --config-file tfsec.yaml --no-color --include-passed
+
+# Capture the exit status code
 echo $? > tfsecstatus.txt
-
-
