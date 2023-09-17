d2145c4dd5f103daf5b9fe6c99003306d579222b
Ankit <43137695+andalike@users.noreply.github.com>
2023-09-17 15:20:58 +0530

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -3,16 +3,16 @@ INSTALL_DIR="$1"
 wget -qO terraform.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
 unzip terraform.zip -d "${INSTALL_DIR}"
 
-# Install TfLint
-wget -qO tflint.zip https://github.com/terraform-linters/tflint/releases/download/v0.47.0/tflint_linux_amd64.zip
-unzip tflint.zip -d "${INSTALL_DIR}"
-mv "${INSTALL_DIR}/tflint" "${INSTALL_DIR}/tflint"
-chmod +x "${INSTALL_DIR}/tflint"
-# Check if 'tflint' is in the PATH
-if ! command -v tflint &> /dev/null; then
-    echo "tflint is not in the PATH. Adding it to PATH temporarily."
-    export PATH="${INSTALL_DIR}:${PATH}"
-fi
+# # Install TfLint
+# wget -qO tflint.zip https://github.com/terraform-linters/tflint/releases/download/v0.47.0/tflint_linux_amd64.zip
+# unzip tflint.zip -d "${INSTALL_DIR}"
+# mv "${INSTALL_DIR}/tflint" "${INSTALL_DIR}/tflint"
+# chmod +x "${INSTALL_DIR}/tflint"
+# # Check if 'tflint' is in the PATH
+# if ! command -v tflint &> /dev/null; then
+#     echo "tflint is not in the PATH. Adding it to PATH temporarily."
+#     export PATH="${INSTALL_DIR}:${PATH}"
+# fi
 
 # Verify that 'tflint' is now available
 tflint --version
